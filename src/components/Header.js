9700cbe4e4b7bd46ea0b4ac71db58cb8d3d73d60
David Lee Vazquez <davidvazquez1026@gmail.com>
2021-08-25 16:39:13 -0500

    added show/hide for task adder section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,11 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({ title }) => {
-
-    const onClick = (e) => {
-        alert('hello')
-    }
-
-
+const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className='header'>
         <h1>{title}</h1>
-       <Button color='green' text='Add' onClick={onClick}/>
+       <Button color={showAdd ? 'Red' : 'Green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
     </header>
   )
 }
